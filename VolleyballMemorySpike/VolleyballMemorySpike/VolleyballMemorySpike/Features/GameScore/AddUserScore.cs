7fc6b93ea0c89cdc6f7d1e05ba6bd33515702aa9
Krzysztof Barbarski <48921668+KrzysztofBarbarski@users.noreply.github.com>
2024-03-08 00:06:47 +0100

    Added AddUserScore command

---
+++ VolleyballMemorySpike/VolleyballMemorySpike/VolleyballMemorySpike/Features/GameScore/AddUserScore.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VolleyballMemorySpike.Abstraction.CQRS;
+using VolleyballMemorySpike.Database.Entities;
+using Volleyballn.Api.Abstraction;
+
+namespace VolleyballMemorySpike.Features.GameScore;
+
+public static class AddUserScore
+{
+    public sealed record Command(
+        Guid SessionId,
+        long Score,
+        long UserId) : ICommand;
+
+    internal sealed class Handler : ICommandHandler<Command>
+    {
+        private readonly IDbContextFactory<ApplicationDbContext> _contextFactory;
+
+        public Handler(IDbContextFactory<ApplicationDbContext> contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public async Task<Result> Handle(Command request, CancellationToken cancellationToken)
+        {
+            using (var context = _contextFactory.CreateDbContext())
+            {
+                var userScore = new UserScore()
+                {
+                    UserId = request.UserId,
+                    SessionId = request.SessionId,
+                    Score = request.Score,
+                };
+
+                await context.AddAsync(userScore);
+                await context.SaveChangesAsync();
+
+                return Result.Success();
+            }
+        }
+    }
+}
+
