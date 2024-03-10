7b011bd6695868d460d679ecec613f41e2267832
Krzysztof Barbarski <48921668+KrzysztofBarbarski@users.noreply.github.com>
2024-03-10 16:00:22 +0100

    added Error class

---
+++ VolleyballMemorySpike/VolleyballMemorySpike/VolleyballMemorySpike/Abstraction/Error.cs
@@ -0,0 +1,8 @@
+﻿namespace Volleyballn.Api.Abstraction;
+
+public record Error(string Code, string Name)
+{
+    public static Error None = new(string.Empty, string.Empty);
+
+    public static Error NullValue = new("Error.NullValue", "Null value was provided");
+}
