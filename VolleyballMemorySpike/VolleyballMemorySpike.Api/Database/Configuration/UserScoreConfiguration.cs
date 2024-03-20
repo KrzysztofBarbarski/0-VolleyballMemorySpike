ebeaa586f9191eadce7d67e39683815505198834
Krzysztof Barbarski <48921668+KrzysztofBarbarski@users.noreply.github.com>
2024-03-20 23:49:17 +0100

    Added UserScoreConfiguration

---
+++ VolleyballMemorySpike/VolleyballMemorySpike.Api/Database/Configuration/UserScoreConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VolleyballMemorySpike.Api.Database.Entities;
+
+namespace VolleyballMemorySpike.Api.Database.Configuration;
+
+public class UserScoreConfiguration : IEntityTypeConfiguration<UserScore>
+{
+    public void Configure(EntityTypeBuilder<UserScore> builder)
+    {
+        builder.ToTable("UserScores");
+
+        builder.HasKey(us => new
+        {
+            us.SessionId,
+            us.UserId,
+            us.CreatedDateUtc
+        });
+
+        builder.HasOne(us => us.User)
+           .WithMany()
+           .HasForeignKey(us => us.UserId)
+           .IsRequired();
+
+        builder.Property(us => us.Score).IsRequired();
+
+        builder.Property(us => us.UserId).IsRequired();
+
+        builder.Property(us => us.SessionId).IsRequired();
+
+        builder.Property(us => us.CreatedDateUtc).IsRequired();
+    }
+}
