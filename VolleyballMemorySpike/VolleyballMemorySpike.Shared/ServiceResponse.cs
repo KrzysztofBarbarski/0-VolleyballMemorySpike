2be383b0e7ca4d7c93e34ba1f6be98b9a850d2a5
Krzysztof Barbarski <48921668+KrzysztofBarbarski@users.noreply.github.com>
2024-03-23 14:00:24 +0100

    Added ServiceResponse

---
+++ VolleyballMemorySpike/VolleyballMemorySpike.Shared/ServiceResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorEcommerce.Shared;
+
+public class ServiceResponse<T>
+{
+    public T? Value { get; set; }
+
+    public bool IsSuccess { get; set; } = true;
+
+    public string Message { get; set; } = string.Empty;
+}
