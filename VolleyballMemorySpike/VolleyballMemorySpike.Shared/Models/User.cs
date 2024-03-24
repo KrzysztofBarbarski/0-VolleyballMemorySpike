160d410804a4940bf8fe9164abe47b80a5763f04
Krzysztof Barbarski <48921668+KrzysztofBarbarski@users.noreply.github.com>
2024-03-24 20:52:38 +0100

    Added Shared project

---
+++ VolleyballMemorySpike/VolleyballMemorySpike.Shared/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VolleyballMemorySpike.Shared.Models
+{
+    public sealed class User
+    {
+        public long Id { get; set; }
+
+        public string ObjectIdentifier { get; set; } = string.Empty;
+
+        [Required]
+        public string NickName { get; set; } = string.Empty;
+
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+        public DateTime CreatedDateUtc { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ VolleyballMemorySpike/VolleyballMemorySpike.Shared/Models/UserScore.cs
@@ -0,0 +1,9 @@
+﻿namespace VolleyballMemorySpike.Shared.Models;
+
+public class UserScore
+{
+    public long Score { get; set; }
+    public string NickName { get; set; }
+
+    public DateTime DateUtc { get; set; }
+}
