5aca033d66be25b3d985145b327a585afd0a79e1
Krzysztof Barbarski <48921668+KrzysztofBarbarski@users.noreply.github.com>
2024-03-25 21:15:15 +0100

    Added UserScore entity

---
+++ VolleyballMemorySpike/VolleyballMemorySpike.Api/Database/Entities/UserScore.cs
@@ -0,0 +1,17 @@
+﻿using VolleyballMemorySpike.Database.Entities.Users;
+
+namespace VolleyballMemorySpike.Api.Database.Entities
+{
+    public sealed class UserScore
+    {
+        public Guid SessionId { get; set; }
+
+        public long Score { get; set; }
+
+        public long UserId { get; set; }
+
+        public User? User { get; set; }
+
+        public DateTime CreatedDateUtc { get; set; } = DateTime.UtcNow;
+    }
+}
