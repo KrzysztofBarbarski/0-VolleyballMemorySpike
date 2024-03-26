167d786842a0c417de2e7dc1f5814fc4a649080b
Krzysztof Barbarski <48921668+KrzysztofBarbarski@users.noreply.github.com>
2024-03-26 17:45:02 +0100

    Added ApplicationDbContext

---
+++ VolleyballMemorySpike/VolleyballMemorySpike.Api/Database/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VolleyballMemorySpike.Api.Database.Entities;
+using VolleyballMemorySpike.Database.Entities.Users;
+
+public sealed class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    public DbSet<UserScore> UsersScores { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
