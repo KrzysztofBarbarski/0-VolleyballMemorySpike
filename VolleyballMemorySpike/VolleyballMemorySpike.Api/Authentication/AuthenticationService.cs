dfee33e21f7020fe8a503dec22473c424c121d97
Krzysztof Barbarski <48921668+KrzysztofBarbarski@users.noreply.github.com>
2024-03-26 17:47:41 +0100

    Updated AuthenticationService

--- VolleyballMemorySpike/VolleyballMemorySpike.Api/Authentication/AuthenticationService.cs
+++ VolleyballMemorySpike/VolleyballMemorySpike.Api/Authentication/AuthenticationService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
+using VolleyballMemorySpike.Database.Entities.Users;
 
 namespace VolleyballMemorySpike.Authentication;
 
@@ -14,6 +15,36 @@ public class AuthenticationService
     {
         _httpContextAccessor = httpContextAccessor;
         _contextFactory = contextFactory;
+    }
+
+    public User? GetCurrentUser()
+    {
+        if (_httpContextAccessor?.HttpContext?.User?.Identity?.IsAuthenticated == false)
+        {
+            return null;
+        }
+
+        var userGuidFromAzure = _httpContextAccessor?.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var userNickName = _httpContextAccessor?.HttpContext?.User?.FindFirst(ClaimTypes.GivenName)?.Value;
+        var userEmail = _httpContextAccessor?.HttpContext?.User?.FindFirst("emails")?.Value;
+
+        // TODO: Refactor and add domain execption
+        if (string.IsNullOrEmpty(userGuidFromAzure) || 
+            string.IsNullOrEmpty(userNickName) || 
+            string.IsNullOrEmpty(userEmail))
+        {
+            throw new Exception();
+        }
+
+        var user = new User()
+        {
+            ObjectIdentifier = userGuidFromAzure,
+            NickName = userNickName,
+            Email = userEmail
+        };
+
+
+        return user;
     }
 
     public async Task<long?> GetCurrentUserId()
